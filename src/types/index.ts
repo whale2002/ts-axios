e29f360c9c59a61dbc15a52f9107d47a8a5adecb
chin <1277180540@qq.com>
2023-01-25 00:41:30 +0800

    feat: 处理header

--- src/types/index.ts
+++ src/types/index.ts
@@ -20,3 +20,13 @@ export interface AxiosRequestConfig {
   data?: any
   params?: any
 }
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+  headers?: any
+}
+
+
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,11 +1,19 @@
-import { AxiosRequestConfig } from './types/index'
+import { AxiosRequestConfig } from "./types"
 
-export function xhr(config: AxiosRequestConfig): void {
-  const { data = null, url, method = 'get' } = config
+export default function xhr(config: AxiosRequestConfig): void {
+  const { data = null, url, method = 'get', headers } = config
 
   const request = new XMLHttpRequest()
 
   request.open(method.toUpperCase(), url, true)
+
+  Object.keys(headers).forEach(name => {
+    if (data === null && name.toLowerCase() === 'content-type') {
+      delete headers[name]
+    } else {
+      request.setRequestHeader(name, headers[name])
+    }
+  })
 
   request.send(data)
 }
