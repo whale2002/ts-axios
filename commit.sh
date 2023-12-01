bb97167bdd31f41ed2f4a76461a789edb94f8267
chin <1277180540@qq.com>
2023-12-02 00:42:29 +0800

    发布ts-axios v1.0

--- commit.sh
+++ commit.sh
@@ -2,14 +2,10 @@
 set -e
 echo "Enter commit message: "
 read COMMIT_MESSAGE
-read -p "Releasing $COMMIT_MESSAGE - are you sure? (y/n)" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  echo "本次commit信息为: $COMMIT_MESSAGE ..."
 
-  # commit
-  git add -A
-  git commit -m "[COMMIT_MESSAGE]"
-  git push origin master
-fi
+echo "本次commit信息为: $COMMIT_MESSAGE"
+
+# commit
+git add -A
+git commit -m "$COMMIT_MESSAGE"
+git push origin master
