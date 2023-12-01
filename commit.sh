07b8d8f9f0b489008131900aa390aa2d1eeae585
chin <1277180540@qq.com>
2023-12-02 00:46:14 +0800

    chore: 删除多余shell文件，修改提交shell脚本

--- commit.sh
+++ commit.sh
@@ -5,7 +5,12 @@ read COMMIT_MESSAGE
 
 echo "本次commit信息为: $COMMIT_MESSAGE"
 
-# commit
-git add -A
-git commit -m "$COMMIT_MESSAGE"
-git push origin master
+read -p "Are you sure? (y/n)" -n 1 -r
+echo  # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # commit
+  git add -A
+  git commit -m "$COMMIT_MESSAGE"
+  git push origin master
+fi
